cdca43be6a45b3842933dc16b9d02b64b717afb2
Marin Calin <marincalin2551@hotmail.com>
2023-11-15 15:47:30 +0200

    Add Sign-up module, service, and routing
    This commit adds a new feature: the Sign-up module, which includes a service for handling HTTP requests for user registration. The commit also adds a routing module for navigating to the Sign-up component. Additionally, a component is created to manage the sign-up form and its validation. Finally, corresponding tests and a request model for the sign-up component and service have also been implemented.

---
+++ src/app/modules/auth/sign-up/sign-up.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { SignUpRoutingModule } from "./sign-up-routing.module";
+import { SignUpComponent } from "./sign-up.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+
+
+@NgModule({
+  declarations: [
+    SignUpComponent
+  ],
+  imports: [
+    CommonModule,
+    SignUpRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule
+  ]
+})
+export class SignUpModule { }
