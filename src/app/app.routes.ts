f5cfba5aeb69a34fa3df0d420ea58b12378c68fa
Marin Calin <marincalin2551@hotmail.com>
2023-11-15 15:47:42 +0200

    Implement authentication guards to app routes
    In this update, authentication guards are introduced to the application routing. This includes the addition of identityFetchedGuard, hasAuthenticationSessionGuard and withoutAuthenticationSessionGuard. The purpose is to present a better-controlled navigation experience depending on user's authentication status. The changes also include the addition of sign-up and sign-in paths for unauthenticated users.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,46 @@
 import { Routes } from "@angular/router";
+import { identityFetchedGuard } from "@tempradar/core/identity/guards/identity-fetched.guard";
+import { hasAuthenticationSessionGuard } from "@tempradar/core/identity/guards/has-authentication-session.guard";
+import {
+  withoutAuthenticationSessionGuard
+} from "@tempradar/core/identity/guards/without-authentication-session.guard";
 
 export const routes: Routes = [
   // Redirect requests to '/' to the home page
   { path: '', redirectTo: 'home', pathMatch: "full" },
 
+  // Application routes
   {
-    path: "home",
-    loadChildren: () => import("@tempradar/modules/home/home.module").then(m => m.HomeModule),
+    path: '',
+    canActivate: [identityFetchedGuard],
+    children: [
+      // Routes that require the user to be authenticated
+      {
+        path: '',
+        canActivate: [hasAuthenticationSessionGuard],
+        children: [
+          { path: "home", loadChildren: () => import("@tempradar/modules/home/home.module").then(m => m.HomeModule) }
+        ]
+      },
+
+      // Routes that require the user to be a guest
+      {
+        path: '',
+        canActivate: [withoutAuthenticationSessionGuard],
+        children: [
+          { path: 'auth/sign-up', loadChildren: () => import("@tempradar/modules/auth/sign-up/sign-up.module").then(m => m.SignUpModule) },
+          { path: 'auth/sign-in', loadChildren: () => import("@tempradar/modules/auth/sign-in/sign-in.module").then(m => m.SignInModule) },
+        ]
+      },
+
+      // Routes accessible regardless of authentication status
+      {
+        path: '',
+        children: [
+
+        ]
+      },
+    ]
   },
 
   {
