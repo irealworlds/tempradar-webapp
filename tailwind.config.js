548fdac50b14594829bc1544dc17eb25fa76564b
Marin Calin <marincalin2551@hotmail.com>
2023-11-15 16:41:48 +0200

    Update primary color palette in Tailwind and Sass configuration
    A new custom primary color palette has been defined in both Tailwind and Sass configuration files. This change allows consistent color scheme throughout the application, replacing the mat.$indigo-palette with a custom set of green tones. It also sets the 'important' flag to true in Tailwind config, ensuring these styles take precedence.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,25 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+  important: true,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': {
+          '50': '#f1fbea',
+          '100': '#def5d2',
+          '200': '#c1ebab',
+          '300': '#99dc7a',
+          '400': '#76cb50',
+          '500': '#52a72f',
+          '600': '#408c24',
+          '700': '#346b20',
+          '800': '#2b561e',
+          '900': '#28491e',
+          '950': '#11280b',
+        },
+      }
+    },
   },
   plugins: [],
 }
