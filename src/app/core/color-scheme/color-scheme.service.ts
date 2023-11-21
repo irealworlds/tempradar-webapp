fbba9bf962188cc3484bba7f67a2b64804931cde
Marin Calin <marincalin2551@hotmail.com>
2023-11-21 17:10:45 +0200

    Refactor ColorSchemeService and remove debug statements
    This commit includes the refactor of the ColorSchemeService by introducing a new type TColorSchemePreference. It also removes the usage of console.debug for a cleaner code base, enhancing maintainability.

--- src/app/core/color-scheme/color-scheme.service.ts
+++ src/app/core/color-scheme/color-scheme.service.ts
@@ -1,19 +1,22 @@
 import { Injectable, Signal } from "@angular/core";
-import { fromEvent, map, startWith, tap } from "rxjs";
+import { fromEvent, map, startWith } from "rxjs";
 import { toSignal } from "@angular/core/rxjs-interop";
+
+export type TColorSchemePreference = "dark" | "light" | undefined;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ColorSchemeService {
-  systemPreference: Signal<"dark" | "light"|undefined>;
+  systemPreference: Signal<TColorSchemePreference>;
 
   constructor() {
+    // Create a signal that holds the current system preference.
     this.systemPreference = toSignal(
       fromEvent(window.matchMedia('(prefers-color-scheme: dark)'), "change").pipe(
-        startWith(window?.matchMedia?.('(prefers-color-scheme:dark)')?.matches ? 'dark' : 'light'),
-        tap(console.debug),
         map((event: any) => event.matches ? "dark" : "light"),
+        startWith(window?.matchMedia?.('(prefers-color-scheme:dark)')?.matches ? 'dark' : 'light'),
+        map((preference: string) => preference as TColorSchemePreference)
       )
     );
   }
