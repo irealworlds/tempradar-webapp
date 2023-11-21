2d0b90fda4ee8eb218a729fa6148bf2d2b58f996
Marin Calin <marincalin2551@hotmail.com>
2023-11-21 18:03:47 +0200

    Refactor fetchHistoryForCityById to use API
    fetchHistoryForCityById method in pinned-city-weather.service is refactored to fetch data from the API, instead of the previously hardcoded values. Loaders are utilized to handle loading states, which improves user experience by providing visual cues during data fetching.

--- src/app/modules/cities/pinned-city-weather.service.ts
+++ src/app/modules/cities/pinned-city-weather.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from "@angular/core";
 import { EnvironmentConfig } from "@tempradar/core/environment/environment-config.model";
 import { HttpClient } from "@angular/common/http";
-import { delay, Observable, of } from "rxjs";
+import { Observable } from "rxjs";
 import { PinnedCityWeatherDetails } from "@tempradar/modules/cities/pinned-city-weather-details.model";
-import {
-  TCityWeatherHistory
-} from "@tempradar/modules/cities/city-details/city-temperature-history-graph/city-temperature-history.type";
 import {
   CityDailyTemperature
 } from "@tempradar/modules/cities/city-details/city-temperature-history-graph/city-daily-temperature.model";
@@ -39,17 +36,8 @@ export class PinnedCityWeatherService {
    * @param {string} cityId - The ID of the city to fetch temperature history for.
    * @return {Observable<Array<CityDailyTemperature>>} - An observable that emits an array of `CityDailyTemperature` objects representing the temperature history for the city.
    */
-  fetchHistoryForCityById(cityId: string): Observable<Array<CityDailyTemperature>> {
-    return of<TCityWeatherHistory>([
-      { date: '2023-01-01', minimumTemperature: -5, maximumTemperature: 5, averageTemperature: 0 },
-      { date: '2023-01-02', minimumTemperature: -4, maximumTemperature: 6, averageTemperature: 1 },
-      { date: '2023-01-03', minimumTemperature: -3, maximumTemperature: 7, averageTemperature: 2 },
-      { date: '2023-01-04', minimumTemperature: -2, maximumTemperature: 8, averageTemperature: 3 },
-      { date: '2023-01-05', minimumTemperature: -1, maximumTemperature: 9, averageTemperature: 4 },
-      { date: '2023-01-06', minimumTemperature: 0, maximumTemperature: 10, averageTemperature: 5 },
-      { date: '2023-01-07', minimumTemperature: 1, maximumTemperature: 11, averageTemperature: 6 },
-    ]).pipe(
-      delay(3000)
-    );
+  fetchHistoryForCityById(cityId: string): Observable<CityDailyTemperature[]> {
+    const endpointUri = new URL(`/PinnedCities/${cityId}/Weather/History`, this._environment.api.baseUri);
+    return this._http.get<CityDailyTemperature[]>(endpointUri.toString());
   }
 }
